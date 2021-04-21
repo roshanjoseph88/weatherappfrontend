539593bf70c929050d36d2c05e3faa144e0c354c
Roshan Joseph <roshanjoseph913@gmail.com>
2021-04-21 22:06:28 +0530

    worked on the subbar

--- src/Content/SubWeather/SubWeather.js
+++ src/Content/SubWeather/SubWeather.js
@@ -1,18 +1,27 @@
 import React from "react";
 import "./Subweather.css";
+import WeatherDetails from "../../Response format/weather.json";
+console.log(WeatherDetails);
 export const SubWeather = () => {
   return (
     <div className="subweather">
       <h3>Weather Details</h3>
       <ul className="subweather-grid">
         <li className="heading">Humidity</li>
-        <li className="heading-details">81%</li>
+        <li className="heading-details">
+          {WeatherDetails.humidityinpercentage}
+        </li>
         <li className="heading">Wind</li>
-        <li className="heading-details">Speed : 6.44 Km/h</li>
+        <li className="heading-details">Speed : {WeatherDetails.wind} Km/h</li>
         <li className="heading">Sunrise and Sunset</li>
-        <li className="heading-details">6.30 am 6.30 am</li>
+        <li className="heading-details">
+          {WeatherDetails.sunrise.hours}:{WeatherDetails.sunrise.min}
+          {WeatherDetails.sunrise.amorpm} &amp; {WeatherDetails.sunset.hours}:
+          {WeatherDetails.sunset.min}
+          {WeatherDetails.sunset.amorpm}
+        </li>
         <li className="heading">Genral Location</li>
-        <li className="heading-details">Bacolod City</li>
+        <li className="heading-details">{WeatherDetails.generallocation}</li>
       </ul>
     </div>
   );
